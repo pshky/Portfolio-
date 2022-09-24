a9f3016cc10274934797ae47e03ab22960d7a1f6
Prasiddha <shakyaprasiddha8@gmail.com>
2022-09-24 19:27:17 +0545

    all about contact components

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
 import './contact.css'
+import {SiGmail} from 'react-icons/si'
+import {RiMessengerLine} from 'react-icons/ri'
+import {FaViber} from 'react-icons/fa'
 const contact = () => {
   return (
-    <section id='contact'>contact</section>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <SiGmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h6>shakyaprasiddha8@gmail.com</h6>
+            <a href='mailto:shakyaprasiddha8@gmail.com' target="_blank">Send a Message</a>
+          </article>
+          <article className='contact__option'>
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Prasiddha Shakya</h5>
+            <a href='https://m.me/prasiddha.shakya1' target="_blank">Send a Message</a>
+          </article>
+          <article className='contact__option'>
+            <FaViber className='contact__option-icon'/>
+            <h4>Viber</h4>
+            <a href='https://api.viber.com/send?phone=+9779840265924' target="_blank">Send a Message</a>
+          </article>
+        </div>
+        {/*=====end of contact options=======*/}
+        <form action=''>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
