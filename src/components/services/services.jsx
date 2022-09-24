09d110d62376e3e49a9007da7826c7c00aefd7f8
Prasiddha <shakyaprasiddha8@gmail.com>
2022-09-24 14:54:15 +0545

    all about services component

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,8 +1,122 @@
 import React from 'react'
 import './services.css'
+import {FcCheckmark} from 'react-icons/fc'
 const services = () => {
   return (
-    <section id='services'>services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>services</h2>
+      <div className='container services__container'>
+        <article className='service'>
+          <div className='service__head'>
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+
+          </ul>
+        </article>
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Web Development</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            
+          </ul>
+        </article>        
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Content-Creation</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            <li>
+              <FcCheckmark className='service__list-icon'/>
+              <p>dsadnjkqwhdjkqw hduiwqhd biqwuhiwqu qiwudb hdiuwq b hduiwqhd</p>
+            </li>
+            
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
 
